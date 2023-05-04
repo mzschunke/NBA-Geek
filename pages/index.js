77032bf6db3a5dacc68b7d71f776cddd5d770dbc
Moritz Zschunke <moritz_z17@web.de>
2023-05-04 12:40:51 +0200

    created Components/teams and implemented SWR hook to render a simple list

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import useSWR from "swr";
+import TeamOverview from "../Components/Teams"; 
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>NBA Geek</h1>
+      <TeamOverview />
     </div>
   );
 }
+
