f554ac254546dfa8fced130525ea92b1fd7780cf
Moritz Zschunke <moritz_z17@web.de>
2023-06-10 11:37:54 +0200

    added DNP logic

--- Components/PlayerStats/index.js
+++ Components/PlayerStats/index.js
@@ -41,6 +41,10 @@ export default function PlayerStats({ id, CURRENT_SEASON }) {
     setPlayerStats(data);
     setShowNoData(data.length === 0);
   }
+  useEffect(() => {
+    console.log("playerStats:", playerStats);
+  }),
+    [playerStats];
 
   async function fetchTeamNames() {
     const response = await fetch(teamNamesURL);
@@ -90,57 +94,59 @@ export default function PlayerStats({ id, CURRENT_SEASON }) {
                 {stat.game.visitor_team_score}{" "}
                 {teamNames[stat.game.visitor_team_id]}
               </StyledParagraph>
-
-              <StatsBox>
-                <thead>
-                  <tr>
-                    <TH>MIN</TH>
-                    <TH>PTS</TH>
-                    <TH>AST</TH>
-                    <TH>REB</TH>
-                    <TH>OREB</TH>
-                    <TH>STL</TH>
-                    <TH>BLK</TH>
-                    <TH>TO</TH>
-                    <TH>PF</TH>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <TD>{stat.min}</TD>
-                    <TD>{stat.pts}</TD>
-                    <TD>{stat.ast}</TD>
-                    <TD>{stat.reb}</TD>
-                    <TD>{stat.oreb}</TD>
-                    <TD>{stat.stl}</TD>
-                    <TD>{stat.blk}</TD>
-                    <TD>{stat.turnover}</TD>
-                    <TD>{stat.pf}</TD>
-                  </tr>
-                  <tr>
-                    <TH>FGM</TH>
-                    <TH>FGA</TH>
-                    <TH>FG%</TH>
-                    <TH>3PM</TH>
-                    <TH>3PA</TH>
-                    <TH>3P%</TH>
-                    <TH>FTM</TH>
-                    <TH>FTA</TH>
-                    <TH>FT%</TH>
-                  </tr>
-                  <tr>
-                    <TD>{stat.fgm}</TD>
-                    <TD>{stat.fga}</TD>
-                    <TD>{stat.fg_pct}</TD>
-                    <TD>{stat.fg3m}</TD>
-                    <TD>{stat.fg3a}</TD>
-                    <TD>{stat.fg3_pct}</TD>
-                    <TD>{stat.ftm}</TD>
-                    <TD>{stat.fta}</TD>
-                    <TD>{stat.ft_pct}</TD>
-                  </tr>
-                </tbody>
-              </StatsBox>
+              {stat.min !== "00" && (
+                <StatsBox>
+                  <thead>
+                    <tr>
+                      <TH>MIN</TH>
+                      <TH>PTS</TH>
+                      <TH>AST</TH>
+                      <TH>REB</TH>
+                      <TH>OREB</TH>
+                      <TH>STL</TH>
+                      <TH>BLK</TH>
+                      <TH>TO</TH>
+                      <TH>PF</TH>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <TD>{stat.min}</TD>
+                      <TD>{stat.pts}</TD>
+                      <TD>{stat.ast}</TD>
+                      <TD>{stat.reb}</TD>
+                      <TD>{stat.oreb}</TD>
+                      <TD>{stat.stl}</TD>
+                      <TD>{stat.blk}</TD>
+                      <TD>{stat.turnover}</TD>
+                      <TD>{stat.pf}</TD>
+                    </tr>
+                    <tr>
+                      <TH>FGM</TH>
+                      <TH>FGA</TH>
+                      <TH>FG%</TH>
+                      <TH>3PM</TH>
+                      <TH>3PA</TH>
+                      <TH>3P%</TH>
+                      <TH>FTM</TH>
+                      <TH>FTA</TH>
+                      <TH>FT%</TH>
+                    </tr>
+                    <tr>
+                      <TD>{stat.fgm}</TD>
+                      <TD>{stat.fga}</TD>
+                      <TD>{stat.fg_pct}</TD>
+                      <TD>{stat.fg3m}</TD>
+                      <TD>{stat.fg3a}</TD>
+                      <TD>{stat.fg3_pct}</TD>
+                      <TD>{stat.ftm}</TD>
+                      <TD>{stat.fta}</TD>
+                      <TD>{stat.ft_pct}</TD>
+                    </tr>
+                  </tbody>
+                </StatsBox>
+              )}
+              {stat.min === "00" && <p>DNP</p>}
             </SingleGame>
           </StatsList>
         ))
