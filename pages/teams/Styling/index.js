567e02e15ebb49aec424b36dafd4b2106efad2ac
Moritz Zschunke <moritz_z17@web.de>
2023-06-13 15:46:15 +0200

    added styling path to team page

---
+++ pages/teams/Styling/index.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const TeamContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 1%;
+  align-items: center;
+  padding-top: 2.5rem;
+  margin: 0 10px 0 10px;
+`;
+
+export const TeamName = styled.h2`
+  font-size: 2.2rem;
+  color: #0d48a0;
+  text-align: center;
+  text-shadow: 1px 1px 0;
+`;
+
+export const StyledTeamDetails = styled.div`
+  text-align: center;
+  font-size: 0.7rem;
+  font-weight: 550;
+`;
