bced5e32191bb3ca00b276eeaec83df50eaee524
Moritz Zschunke <moritz_z17@web.de>
2023-06-14 15:56:50 +0200

    added logos to gameslist

--- Components/GamesDisplay/GamesListContainer.js
+++ Components/GamesDisplay/GamesListContainer.js
@@ -1,5 +1,6 @@
 import { GamesList, SingleGame, StyledDate } from "./Styling";
 import { NoData } from "@/styles";
+import Image from "next/image";
 
 export function GamesListContainer({ games, noDataMessage }) {
   return (
@@ -12,8 +13,23 @@ export function GamesListContainer({ games, noDataMessage }) {
                 {game.date.split("T")[0]}
                 {": "}
               </StyledDate>
+              <Image
+                src={`/images/team-logos/${game.home_team.id}.png`}
+                width={25}
+                height={25}
+                style={{ objectFit: "contain" }}
+                alt={game.home_team.full_name}
+              />
               {game.home_team.full_name} {game.home_team_score} -{" "}
-              {game.visitor_team_score} {game.visitor_team.full_name}
+              {game.visitor_team_score}
+              <Image
+                src={`/images/team-logos/${game.visitor_team.id}.png`}
+                width={25}
+                height={25}
+                style={{ objectFit: "contain" }}
+                alt={game.visitor_team.full_name}
+              />
+              {game.visitor_team.full_name}
             </SingleGame>
           </GamesList>
         ))
